aced7f05315a239d05094ed93e57987f49d52fa7
Hanna Chen <hanna@rethinkcanada.com>
2017-02-27 21:00:26 -0500

    Initial Commit

---
+++ Week7 HomeworkTests/Week7_HomeworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  Week7_HomeworkTests.swift
+//  Week7 HomeworkTests
+//
+//  Created by Hanna Chen on 2/27/17.
+//  Copyright © 2017 Rethink Canada. All rights reserved.
+//
+
+import XCTest
+@testable import Week7_Homework
+
+class Week7_HomeworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
