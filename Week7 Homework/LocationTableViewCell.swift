4bdc4485d03d0b76b85dcee80ae0cdfce61d792e
Hanna Chen <hanna@rethinkcanada.com>
2017-03-06 23:33:13 -0500

    Add missing data file A couple of files and changes didn't make it into the repo.

--- Week7 Homework/LocationTableViewCell.swift
+++ Week7 Homework/LocationTableViewCell.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-class LocationTableViewCell: UITableViewCell {
+class LocationTableViewCell: UITableViewCell, UITableViewDelegate {
+    
+    // Constants
+    let defaultButtonTextColor: UIColor = #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)
+    let defaultButtonBackgroundColor: UIColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    let highlightButtonTextColor: UIColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+    let highlightButtonBackgroundColor: UIColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
+    let visitedText: String = "Visited"
+    let markVisitedText: String = "Mark as Visited"
 
     // Outlets
     @IBOutlet var locationLabel: UILabel!
@@ -16,42 +24,53 @@ class LocationTableViewCell: UITableViewCell {
     @IBOutlet var checkButton: UIButton!
     
     // Properties
-    
-    // Other
-    var defaultButtonTextColor: UIColor = #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)
-    var defaultButtonBackgroundColor: UIColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+    var rowIndex: Int?
+    var delegate: LocationTableViewCellDelegate?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
         
         // Prevent background colours from clearing on cell select
         self.selectionStyle = UITableViewCellSelectionStyle.none
     }
     
     // On check button click
-    @IBAction func checkButton(_ sender: UIButton) {
-        // Update button status (checkButton)
-        self.checkButton.backgroundColor = UIColor.red
-//        self.checkButton.setImage(UIImage(named: "play.png"), for: UIControlState.normal)
+    @IBAction func checkButtonClick(_ sender: UIButton) {
+        
+        // Delegate click back to the controller
+        // REMEMBER -- Don't mutate data in view!! This file is a VIEW!
+        if let delegate = self.delegate {
+            
+            delegate.cellToggleButton(cell: self)
+        }
     }
     
     func setupCellWith(location: Location) {
+            
         // Populate labels with data
         self.locationLabel.text = location.name
         self.locationImageView.image = location.image
         
-        // Set button style
-        let checkButtonTextColor: UIColor = location.visited ? UIColor.green : self.defaultButtonTextColor
-        let checkButtonBackgroundColor: UIColor = location.visited ? UIColor.white : self.defaultButtonBackgroundColor
-        self.checkButton.setTitleColor(checkButtonTextColor, for: UIControlState.normal)
+        // Update button style
+        self.updateButton(visited: location.visited)
+    }
+    
+    func updateButton(visited: Bool) {
+    
+        // Set button state
+        let checkButtonTextColor: UIColor = visited ? self.highlightButtonTextColor : self.defaultButtonTextColor
+        let checkButtonBackgroundColor: UIColor = visited ? self.highlightButtonBackgroundColor : self.defaultButtonBackgroundColor
+    
+        // Set styles
         self.checkButton.backgroundColor = checkButtonBackgroundColor
+        self.checkButton.setTitleColor(checkButtonTextColor, for: .normal)
+        
+        // Set text label
+        self.checkButton.setTitle(visited ? self.visitedText : self.markVisitedText, for: .normal)
     }
     
 }
